68a07be85b8ba7a72b455f7bd5cf212b7c5feeb4
Damian Drewulski (damiannelus) <damiannelus@gmail.com>
2021-02-14 00:35:01 +0100

    aligned with the project's requirements

---
+++ mouse_move_fingerprint/src/components/mouse_trackpad.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="mouse_track_pad">Foo bar</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class MouseTrackPad extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#mouse_track_pad {
+    display: flexbox;
+    background-color: blueviolet;
+    height: 100%;
+    min-height: 100%;
+	flex-direction: column;
+}
+</style>
